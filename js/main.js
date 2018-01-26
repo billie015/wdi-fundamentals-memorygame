f01f7eaa3f5b7a3041528bf63144ae19f6b3f548
Billie <billie.oller@gmail.com>
2018-01-25 17:48:30 -0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var flipCard = function(cardId) {
+console.log ("User flipped" + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+}
+
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  }
+  else {
+    alert("Sorry try again");
+  }
+};
+
+checkForMatch();
+flipCard(0);
+flipCard(2);
